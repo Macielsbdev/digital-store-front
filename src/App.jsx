d1bc273b6116bd2c22f1f0dc4db28469e0797b77
Macielsbdev <macielbezerra2018@gmail.com>
2025-05-13 22:25:39 -0300

    adicionei uma section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
+import SectionBanner from "./components/SectionBanner";
 
 const App = () => {
   return (
     <>
       <Header />
+      <SectionBanner />
+      
     </>
+    
   );
 }
 
